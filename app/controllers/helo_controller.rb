b3556c9453b97ec7791c0c1893567e7dec690c5d
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-15 04:35:35 +0000

    check_box,labelのタグ利用

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,17 +3,15 @@ class HeloController < ApplicationController
   def index
     if request.post?
       @title = 'Result'
-      @msg = 'you typed: ' + params[:input1] + '.'
-      @value = params[:input1]
+      if params['check1']
+        @msg = 'you Checked!!'
+      else
+        @msg = 'not checked...'
+      end
     else
       @title = 'Index'
-      @msg = 'type text...'
-      @value = ''
+      @msg = 'check it...'
     end
   end
   
-  def other
-    redirect_to action: :index, params: {msg: 'from other page'}
-  end
-
 end
