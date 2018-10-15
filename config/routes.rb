97e80e0f6a67da64f0a1ff936c0d0181db74d865
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-15 03:54:28 +0000

    ルーティングをget→postに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'helo' => 'helo#index'
-  get 'helo/other'
+  post 'helo' => 'helo#index'
+  post 'helo/index'
   
 end
