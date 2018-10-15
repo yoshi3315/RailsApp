d557236549d6c57f52ae8fd9bfcaa6a88b597e2e
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-15 05:43:59 +0000

    selectのタグ利用

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -3,14 +3,14 @@ class HeloController < ApplicationController
   def index
     if request.post?
       @title = 'Result'
-      if params['r1']
-        @msg = 'you selected: ' + params[:r1]
+      if params['s1']
+        @msg = 'you selected: ' + params[:s1]
       else
         @msg = 'not selected...'
       end
     else
       @title = 'Index'
-      @msg = 'select radio button...'
+      @msg = 'select List...'
     end
   end
   
