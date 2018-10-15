7fb0eeb94d7d0b9e2f0cc130a217430437565471
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-15 03:56:43 +0000

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'helo' => 'helo#index'
+  get 'helo/index'
+  
   post 'helo' => 'helo#index'
   post 'helo/index'
   
