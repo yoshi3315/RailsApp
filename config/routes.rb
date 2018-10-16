e9afd7a6c27a8b8d497a2d15d4408203e83b0be2
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-16 09:24:54 +0000

    rails g controller people indexの実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'people/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'helo', to: 'helo#index'
