949f25a3cdecabe7f0f5329d742d4644bbe57eea
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-16 10:02:10 +0000

    /peopleへのアクセスでもpeople#indexが実行されるようにルーティング記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'people/index'
+  get 'people', to: 'people#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
