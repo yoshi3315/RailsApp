fe6aa83d32220368b7ca51b92485a157d5e76723
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-17 02:46:33 +0000

    /people/add.html表示用のaddアクションとDB書き込み用のcreateアクションを記載

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,4 +10,21 @@ class PeopleController < ApplicationController
     @data = Person.find(params[:id])
   end
 
+  def add
+    @msg = 'add new data.'
+  end
+  
+  protect_from_forgery
+  
+  def create
+    if request.post?
+      obj = Person.create(
+        name: params[:name],
+        age: params[:age],
+        mail: params[:mail]
+      )
+    end
+    redirect_to('/people')
+  end
+
 end
