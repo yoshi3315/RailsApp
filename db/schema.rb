58f1af4742420029d28f4ee403d5762ac86f7701
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-18 05:09:45 +0000

    rails g model card title:text author:text price:integer publisher:text memo:textの実行とrails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181016084818) do
+ActiveRecord::Schema.define(version: 20181018050747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.text     "title"
+    t.text     "author"
+    t.integer  "price"
+    t.text     "publisher"
+    t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.text     "name"
