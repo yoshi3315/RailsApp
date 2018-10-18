ea5ca1bedf8684d20fea314cf562dd596eaf7f01
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-18 05:13:31 +0000

    rails g controller cards index show add editの実行

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,13 @@
+class CardsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def add
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+
+  get 'cards/show'
+
+  get 'cards/add'
+
+  get 'cards/edit'
+
   get 'people', to: 'people#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
