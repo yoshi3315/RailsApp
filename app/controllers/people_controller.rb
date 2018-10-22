e03dacacdad15da2d6e0333c71ccadee7d9e4538
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-22 01:19:09 +0000

    名前、メールアドレスの部分一致検索ができるようにpeople#findアクションを変更

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -39,8 +39,8 @@ class PeopleController < ApplicationController
     @msg = 'please type search word...'
     @people = Array.new
     if request.post?
-      f = params[:find].split(',')
-      @people = Person.where('age >= ? and age <= ?', f[0], f[1])
+      f = '%' + params[:find] + '%'
+      @people = Person.where('name like ? or mail like ?', f, f)
     end
   end
 
