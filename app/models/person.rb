f03562e0ed862af6d6a460c61a772e2b83307d51
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-22 07:35:51 +0000

    Personモデルのバリデーションに引っかかった場合のエラーメッセージを指定

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,4 @@
 class Person < ApplicationRecord
-  validates :name, presence: true
-  validates :age, numericality: true
+  validates :name, presence: { message: 'は、必須項目です。' }
+  validates :age, numericality: { message: 'は、数字で入力してください。' }
 end
