37ffac6206ed7e239ef87717de51f0d399771159
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-22 08:16:46 +0000

    rails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181018050747) do
+ActiveRecord::Schema.define(version: 20181022081451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20181018050747) do
     t.integer  "price"
     t.text     "publisher"
     t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer  "person_id"
+    t.text     "title"
+    t.text     "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
