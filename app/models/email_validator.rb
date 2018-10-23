3a38f51c0afa03b49aecb83f8e7760dd03fba7c2
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-23 04:30:22 +0000

    i18nの設定

--- app/models/email_validator.rb
+++ app/models/email_validator.rb
@@ -2,7 +2,7 @@ class EmailValidator < ActiveModel::EachValidator
   
   def validate_each(record, attribute, value)
     unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || 'is not an email')
+      record.errors[attribute] << (options[:message] || 'は所定の形式ではありません')
     end
   end
   
