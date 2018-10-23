9f990610d9e77ab6b69ed08fb7ccd57748004db7
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-23 09:40:56 +0000

    blogconfigs#editアクション、ストロングパラメーターのメソッドを作成

--- app/controllers/blogconfigs_controller.rb
+++ app/controllers/blogconfigs_controller.rb
@@ -7,5 +7,15 @@ class BlogconfigsController < ApplicationController
   end
 
   def edit
+    @blogconfigs = Blogconfig.find(1)
+    if @blogconfigs.update(blogconfig_params)
+      redirect_to('/blogconfigs')
+    end
   end
+
+  private
+  def blogconfig_params
+    params.require(:blogconfig).permit(:title, :subtitle, :stylename)
+  end
+  
 end
