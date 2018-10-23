e0069338c7eb291bbedf305cc618d871825b390b
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-23 05:26:37 +0000

    rails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181022081451) do
+ActiveRecord::Schema.define(version: 20181023052513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "blogconfigs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "subtitle"
+    t.text     "stylename"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "bloggenres", force: :cascade do |t|
+    t.text     "name"
+    t.text     "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogposts", force: :cascade do |t|
+    t.text     "title"
+    t.text     "read"
+    t.text     "content"
+    t.integer  "bloggenre_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "cards", force: :cascade do |t|
     t.text     "title"
