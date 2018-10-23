aa3c5555675e9cc3398ffdb74fe44efec156aff9
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-23 05:30:33 +0000

    シードファイル作成のため、db/seeds.rb記載とrails db:seedの実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@
 Person.create(name: 'Taro', age:38, mail: 'taro@yamada')
 Person.create(name: 'Hanako', age:34, mail: 'hanako@flower')
 Person.create(name: 'sachiko', age:56, mail: 'sachiko@happy')
+
+Blogconfig.create(id: 1, title: 'my blog', subtitle: 'サンプルで作ったブログです。', stylename: 'gray')
