6e03b8305fe6b5e1ea54bdbfaa23ddc27ed96666
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-23 05:41:04 +0000

    rails g controller blogconfigs index editの実行

---
+++ app/controllers/blogconfigs_controller.rb
@@ -0,0 +1,7 @@
+class BlogconfigsController < ApplicationController
+  def index
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'blogconfigs/index'
+
+  get 'blogconfigs/edit'
+
   get 'messages/index'
 
   get 'messages/show'
