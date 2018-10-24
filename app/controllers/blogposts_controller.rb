fe96fe3a28ad7e00b20fadf9d05585fcc5624daf
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-24 01:09:19 +0000

    blogpostsコントローラーでDRYに注意しメソッドをまとめる

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,6 +1,8 @@
 class BlogpostsController < ApplicationController
 
   layout 'blogposts'
+  before_action :set_blogpost, only: [ :edit, :destroy ]
+  before_action :set_bloggenres, only: [ :new, :edit ]
   
   def index
     @blogposts = Blogpost.all.order('created_at desc')
@@ -8,35 +10,43 @@ class BlogpostsController < ApplicationController
 
   def new
     @blogpost = Blogpost.new
-    @bloggenres = Bloggenre.all
   end
 
   def create
     if @blogpost = Blogpost.create(blogpost_params)
-      redirect_to('/blogposts')
+      go_back
     end
   end
 
   def edit
-    @blogpost = Blogpost.find(params[:id])
-    @bloggenres = Bloggenre.all
   end
   
   def update
     if @blogpost.update(blogpost_params)
-      redirect_to('/blogposts')
+      go_back
     end
   end
 
   def destroy
-    @blogpost = Blogpost.find(params[:id])
     @blogpost.destroy
-    redirect_to('/blogposts')
+    go_back
   end
   
   private
   def blogpost_params
     params.require(:blogpost).permit(:title, :read, :content, :bloggenre_id)
+  end
+
+  def set_blogpost
+    @blogpost = Blogpost.find(params[:id])
+  end
+
+  def set_bloggenres
+    @bloggenres = Bloggenre.all
+  end
+
+  def go_back
+    redirect_to('/blogposts')
   end
 
 end
