5ea67281c01d47a8f6c8da10ce8aa28786ec88c2
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-24 02:35:00 +0000

    blogs#genreアクションを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,6 +15,17 @@ class BlogsController < ApplicationController
   end
 
   def genre
+    page_size = 5
+    @page_num = 0
+    if params[:page]
+      @page_num = params[:page].to_i
+    end
+    @bloggenre = Bloggenre.find(params[:id])
+    @blogpost = Blogpost.where('bloggenre_id = ?', params[:id])
+      .order('created_at desc')
+      .offset(page_size * @page_num)
+      .limit(page_size)
+    @blogconfig = Blogconfig.find(1)
   end
 
   def show
