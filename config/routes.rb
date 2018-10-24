3faf9c42dbce4561161b469a662fa01de22d60a5
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-24 04:15:30 +0000

    ここまでのアクションに対するルーティングを記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,23 @@
 Rails.application.routes.draw do
 
-  get 'blogs/index'
+  get 'blogs', to: 'blogs#index'
+  get 'blogs/:id/genre', to: 'blogs#genre'
+  get 'blogs/:id/:page/genre', to: 'blogs#genre'
+  get 'blogs/:id/show', to: 'blogs#show'
+  get 'blogs/:page', to: 'blogs#index'
 
-  get 'blogs/genre'
+  get 'blogposts/:id/delete', to: 'blogposts#delete'
+  resources :blogposts
 
-  get 'blogs/show'
-
-  get 'blogposts/index'
-
-  get 'blogposts/new'
-
-  get 'blogposts/edit'
-
-  get 'blogposts/delete'
-
-  get 'bloggenres/index'
-
+  get 'bloggenres', to: 'bloggenres#index'
   get 'bloggenres/new'
+  post 'bloggenres/create'
+  get 'bloggenres/:id/edit', to: 'bloggenres#edit'
+  patch 'bloggenres/:id/update', to: 'bloggenres#update'
 
-  get 'bloggenres/edit'
-
-  get 'blogconfigs/index'
-
+  get 'blogconfigs', to: 'blogconfigs#index'
   get 'blogconfigs/edit'
-
-  get 'messages/index'
-
-  get 'messages/show'
-
-  get 'messages/new'
-
-  get 'messages/edit'
+  patch 'blogconfigs/update'
 
   get 'helo', to: 'helo#index'
   post 'helo', to: 'helo#index'
