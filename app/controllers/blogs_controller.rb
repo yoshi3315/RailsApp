a4555feba82f376cbfca2fa60e6d428c899a3a33
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-24 02:41:11 +0000

    blogsコントローラーのDRYに注意し修正

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,35 +1,39 @@
 class BlogsController < ApplicationController
   
   layout 'blogs'
+  before_action :set_page_num, only: [ :index, :genre ]
   
   def index
-    page_size = 5
-    @page_num = 0
-    if params[:page]
-      @page_num = params[:page].to_i
-    end
     @blogposts = Blogpost.all.order('created_at desc')
       .offset(page_size * @page_num)
       .limit(page_size)
-    @blogconfig = Blogconfig.find(1)
+    set_blogconfig
   end
 
   def genre
-    page_size = 5
-    @page_num = 0
-    if params[:page]
-      @page_num = params[:page].to_i
-    end
     @bloggenre = Bloggenre.find(params[:id])
     @blogpost = Blogpost.where('bloggenre_id = ?', params[:id])
       .order('created_at desc')
       .offset(page_size * @page_num)
       .limit(page_size)
-    @blogconfig = Blogconfig.find(1)
+    set_blogconfig
   end
 
   def show
     @blogpost = Blogpost.find(params[:id])
+    set_blogconfig
+  end
+  
+  private
+  def set_page_num
+    page_size = 5
+    @page_num = 0
+    if params[:page]
+      @page_num = params[:page].to_i
+    end
+  end
+  
+  def set_blogconfig
     @blogconfig = Blogconfig.find(1)
   end
   
