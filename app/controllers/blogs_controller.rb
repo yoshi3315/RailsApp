abe0f4bf63defae01e720493388ac7b8305e1de6
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-24 02:30:38 +0000

    blogs#indexアクションを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,6 +3,15 @@ class BlogsController < ApplicationController
   layout 'blogs'
   
   def index
+    page_size = 5
+    @page_num = 0
+    if params[:page]
+      @page_num = params[:page].to_i
+    end
+    @blogposts = Blogpost.all.order('created_at desc')
+      .offset(page_size * @page_num)
+      .limit(page_size)
+    @blogconfig = Blogconfig.find(1)
   end
 
   def genre
