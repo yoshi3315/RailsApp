d73070a3c8f035dfa2b581d4037e5df67bb89ed7
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-25 04:33:09 +0000

    余計なページにアクセスしないようquestions#edit,update,destroyはindexへリダイレクト設定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,6 +22,7 @@ class QuestionsController < ApplicationController
 
   # GET /questions/1/edit
   def edit
+    redirect_to questions_path
   end
 
   # POST /questions
@@ -43,25 +44,13 @@ class QuestionsController < ApplicationController
   # PATCH/PUT /questions/1
   # PATCH/PUT /questions/1.json
   def update
-    respond_to do |format|
-      if @question.update(question_params)
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { render :show, status: :ok, location: @question }
-      else
-        format.html { render :edit }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to questions_path
   end
 
   # DELETE /questions/1
   # DELETE /questions/1.json
   def destroy
-    @question.destroy
-    respond_to do |format|
-      format.html { redirect_to questions_url, notice: 'Question was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+   redirect_to questions_path
   end
 
   private
