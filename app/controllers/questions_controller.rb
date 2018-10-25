d46bbcda94449d17228eb6af734c6a9db8b81bde
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-25 04:22:05 +0000

    質問一覧を降順で表示するようにquestions#indexアクションを修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = Question.all
+    @questions = Question.all.order created_at: :desc
   end
 
   # GET /questions/1
