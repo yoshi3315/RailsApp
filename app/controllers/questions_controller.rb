a8a96edd09c24d65c0d41a2958dd781632616e63
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-25 04:20:22 +0000

    questionsコントローラーでレイアウトを指定

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
+  layout 'questions'
 
   # GET /questions
   # GET /questions.json
