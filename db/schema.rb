6debaa1878cfcf996467a453d39fe9e922b6488b
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-25 08:47:29 +0000

    rails db:migrateの実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024110522) do
+ActiveRecord::Schema.define(version: 20181025084615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,36 @@ ActiveRecord::Schema.define(version: 20181024110522) do
     t.text     "mail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questionaries", force: :cascade do |t|
+    t.text     "title"
+    t.text     "description"
+    t.datetime "deadline"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "questionary_choices", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "value"
+    t.integer  "questionary_item_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "questionary_items", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "questionary_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "questionary_results", force: :cascade do |t|
+    t.integer  "questionary_id"
+    t.text     "result"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "questions", force: :cascade do |t|
