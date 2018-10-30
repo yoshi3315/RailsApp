df17a540a1457861aa53e79fdbc7095cf45a107d
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 10:34:30 +0000

    DRYに注意しboard_messagesコントローラーを修正

--- app/controllers/board_messages_controller.rb
+++ app/controllers/board_messages_controller.rb
@@ -1,11 +1,10 @@
 class BoardMessagesController < ApplicationController
-  before_action :set_board_message, only: [:show, :edit, :update, :destroy]
+  before_action :set_board_messages, only: [:index, :create]
   before_action :authenticate_account!
   
   # GET /board_messages
   # GET /board_messages.json
   def index
-    @board_messages = BoardMessage.page([:page]).order('created_at desc')
     users = BoardUser.where(account_id: current_account.id)
     if users[0] == nil
       user = BoardUser.new
@@ -19,24 +18,9 @@ class BoardMessagesController < ApplicationController
     @board_message.board_user_id = @board_user.id
   end
 
-  # GET /board_messages/1
-  # GET /board_messages/1.json
-  def show
-  end
-
-  # GET /board_messages/new
-  def new
-    @board_message = BoardMessage.new
-  end
-
-  # GET /board_messages/1/edit
-  def edit
-  end
-
   # POST /board_messages
   # POST /board_messages.json
   def create
-    @board_messages = BoardMessage.page([:page]).order('created_at desc')
     @board_message = BoardMessage.new(board_message_params)
     @board_user = BoardUser.where(account_id: current_account.id)
 
@@ -51,34 +35,10 @@ class BoardMessagesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /board_messages/1
-  # PATCH/PUT /board_messages/1.json
-  def update
-    respond_to do |format|
-      if @board_message.update(board_message_params)
-        format.html { redirect_to @board_message, notice: 'Board message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @board_message }
-      else
-        format.html { render :edit }
-        format.json { render json: @board_message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /board_messages/1
-  # DELETE /board_messages/1.json
-  def destroy
-    @board_message.destroy
-    respond_to do |format|
-      format.html { redirect_to board_messages_url, notice: 'Board message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_board_message
-      @board_message = BoardMessage.find(params[:id])
+    def set_board_messages
+      @board_messages = BoardMessage.page([:page]).order('created_at desc')
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
