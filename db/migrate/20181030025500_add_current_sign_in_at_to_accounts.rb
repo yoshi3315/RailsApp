29d14dc5135c9590fced5679ff5f63cbe2f061fb
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 02:58:50 +0000

    migrationファイル名を間違ったので修正

---
+++ db/migrate/20181030025500_add_current_sign_in_at_to_accounts.rb
@@ -0,0 +1,4 @@
+class AddCurrentSignInAtToAccounts < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/migrate/20181030025500_add_current_sign_in_at_to_devise_create_accounts.rb
+++
@@ -1,4 +0,0 @@
-class AddCurrentSignInAtToDeviseCreateAccounts < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
