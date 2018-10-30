6a43ea59ed28991f901b14fb218fe21e46204004
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 01:56:02 +0000

    Yahooの場合httpsにリダイレクトされるので、httpで接続でき るTwitterのハッシュタグRSSを表示するように変更

--- app/controllers/ajax_controller.rb
+++ app/controllers/ajax_controller.rb
@@ -7,7 +7,7 @@ class AjaxController < ApplicationController
   end
   
   def data
-    url = URI('http://news.yahoo.co.jp/pickup/rss.xml')
+    url = URI('http://queryfeed.net/twitter?q=%E3%83%8B%E3%83%83%E3%82%B9%E3%82%A4+EPA&title-type=user-name-both')
     http = Net::HTTP.new(url.host)
     response = http.get(url)
     @data = Hash.from_xml(response.body).to_json.html_safe
