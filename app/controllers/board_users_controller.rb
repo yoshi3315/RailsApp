db6fcd86bd5c7a18b20159b9ccac910742d09981
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 09:42:50 +0000

    broad_usersコントローラーにautheticate_accountメソッドを追加

--- app/controllers/board_users_controller.rb
+++ app/controllers/board_users_controller.rb
@@ -1,6 +1,7 @@
 class BoardUsersController < ApplicationController
   before_action :set_board_user, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_account!
+  
   # GET /board_users
   # GET /board_users.json
   def index
