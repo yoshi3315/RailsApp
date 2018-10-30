41798f1a84c47ea03627bd22cd588902a807a487
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 02:37:10 +0000

    heloコントローラーを修正し不要な記述削除、login_checkアクション、beforeアクションの設定

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,11 +1,14 @@
 class HeloController < ApplicationController
   layout 'helo'
+  before_action :authenticate_account!, only: :login_check
   
   def index
-    @header = 'layout sample'
-    @footer = 'copyright SYODA-Tuyano 2016.'
-    @title = 'New Layout'
     @msg = 'this is sample page'
+  end
+  
+  def login_check
+    @account = current_account
+    @msg = 'you logined at: ' + @account.current_sign_in_at.to_s
   end
   
 end
