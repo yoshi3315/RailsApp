1f86a331fd1e77c88cee0eba0eb2fcc39e6c987f
[yoshi3315] <[yoshiki.chiba@one-star.jp]>
2018-10-30 04:25:33 +0000

    シードファイルの作成とrails db:seedの実行

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Person.create(name: 'Taro', age:38, mail: 'taro@yamada')
-Person.create(name: 'Hanako', age:34, mail: 'hanako@flower')
-Person.create(name: 'sachiko', age:56, mail: 'sachiko@happy')
+#Person.create(name: 'Taro', age:38, mail: 'taro@yamada')
+#Person.create(name: 'Hanako', age:34, mail: 'hanako@flower')
+#Person.create(name: 'sachiko', age:56, mail: 'sachiko@happy')
 
-Blogconfig.create(id: 1, title: 'my blog', subtitle: 'サンプルで作ったブログです。', stylename: 'gray')
+#Blogconfig.create(id: 1, title: 'my blog', subtitle: 'サンプルで作ったブログです。', stylename: 'gray')
+
+data = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'zero', 'white', 'black', 'gray', 'red', 'blue', 'green', 'yellow', 'cyan', 'magenda']
+
+(1..100).each do |i|
+  DataPage.create data:data.sample
+end
